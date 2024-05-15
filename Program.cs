7868ba0d34061589073687b153500a615ec01521
Andrea Silvestre <andrea.silvestre@yahoo.com.br>
2024-05-15 15:24:08 -0300

    Desafio POO entregando

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Modelo numero 123", imei: "abs25364", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4567895", modelo: "Modelo numero 250", imei: "ADF25364", memoria: 120);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
